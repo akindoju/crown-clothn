4c03f641b3a5722f8154e21323c22fe711720113
Akndju <davidd.akindoju@gmail.com>
2021-01-04 15:23:30 +0100

    Created mobile view for entire app

--- client/src/pages/sign-in-sign-up/sign-in-sign-up.styles.jsx
+++ client/src/pages/sign-in-sign-up/sign-in-sign-up.styles.jsx
@@ -5,4 +5,15 @@ export const ShopPageContainer = styled.div`
   display: flex;
   justify-content: space-between;
   margin: 30px auto;
+
+  @media screen and (max-width: 800px) {
+    overflow: hidden;
+    display: block;
+    width: unset;
+    align-items: center;
+    justify-content: unset;
+
+    > *:first-child {
+      margin-bottom: 50px;
+    }
 `;
