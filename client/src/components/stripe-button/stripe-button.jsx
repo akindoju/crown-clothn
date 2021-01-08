a9bdbf3ffea47a6506f994637009b0493827c4ad
Akndju <davidd.akindoju@gmail.com>
2021-01-08 14:45:41 +0100

    Updating saga code to create cart based on user

--- client/src/components/stripe-button/stripe-button.jsx
+++ client/src/components/stripe-button/stripe-button.jsx
@@ -33,7 +33,7 @@ const StripeCheckoutButton = ({ price }) => {
       name="Crwn Clothing Ltd."
       billingAddress
       shippingAddress
-      image="https://svgshare.com/i/CUz.svg"
+      image="https://sendeyo.com/up/d/f3eb2117da"
       description={`Your total is ${price}`}
       amount={priceForStripe}
       token={onToken}
