17d7ed77556bca340254371cf9872b51f6c3d197
Akindoju Olusola <59252402+akindoju@users.noreply.github.com>
2023-06-12 19:07:31 +0100

    Update firebase.utils.js

--- client/src/firebase/firebase.utils.js
+++ client/src/firebase/firebase.utils.js
@@ -3,14 +3,7 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: 'AIzaSyBeYe5pwJAuwpNXpW45a5dPPojPDqpSKsI',
-  authDomain: 'crwn-db-49ac6.firebaseapp.com',
-  databaseURL: 'https://crwn-db-49ac6.firebaseio.com',
-  projectId: 'crwn-db-49ac6',
-  storageBucket: 'crwn-db-49ac6.appspot.com',
-  messagingSenderId: '841134974864',
-  appId: '1:841134974864:web:8696515c7ce417dc957515',
-  measurementId: 'G-764XPY488J',
+  //Put firebase config here
 };
 
 firebase.initializeApp(config);
